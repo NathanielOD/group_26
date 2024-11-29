98cf8405132e159cd92c5ed27220692599e5dd80
baskamphorst <160472071+baskamphorst@users.noreply.github.com>
2024-11-29 15:44:02 +0100

    Input en output in goede format

--- testing.py
+++ testing.py
@@ -27,40 +27,62 @@ def get_wikipedia_url(entity):
 
 
 
-# Prompts
-prompt_input = ("What is the capital of Egypt?")
+# fill in the file that contains the question in the desired format
+fn = "example_input.txt"
+
+# extract questions from the file
+question_ids = []
+questions = []
+
+# Open the file in read mode
+with open(fn, 'r') as file:
+    # Read each line in the file
+    for line in file:
+        # Split the line by the tab character '\t'
+        parts = line.strip().split('\t')
+        if len(parts) == 2:  # Ensure the line has exactly two parts
+            question_id, question = parts
+            question = question.removeprefix("Question: ").removesuffix(" Answer:")
+            question_ids.append(question_id)
+            questions.append(question)
 
 
 
 
+# define the model
 llm = Llama(model_path=model_path, verbose=False)
 
 # Generate LLM response based on optimized parameters
-output = llm(
-      prompt_input, # Prompt
-      max_tokens = 32,# Generate up to 32 tokens
-      temperature = 0.6,
-      top_p = 0.2,
-      stop=["Q:"], # Stop generating just before the model would generate a new question
-      echo=False #  Do not echo the prompt back in the output
-)
+for i,id in enumerate(question_id):
+    question = questions[i]
+    output = llm(
+          question, # Prompt
+          max_tokens = 32,# Generate up to 32 tokens
+          temperature = 0.6,
+          top_p = 0.2,
+          stop=["Q:"], # Stop generating just before the model would generate a new question
+          echo=False #  Do not echo the prompt back in the output
+    )
 
-full_response = ""
+    response = output['choices'][0]['text']
 
+    print("\n \n The LLaMA response is: \n", response)
 
+    doc = nlp(prompt_input + " " + response)
 
-print("\n \n The LLaMA response is: \n", output['choices'][0]['text'])
+    # extract the entities
+    entities = []
 
-doc = nlp(prompt_input + " " + output['choices'][0]['text'])
+    for ent in doc.ents:
+        entities.append((ent.text,ent.label_,get_wikipedia_url(ent.text)))
 
-entities = []
+    # keep only unique entities
+    entities = list(set(entities))
 
-for ent in doc.ents:
-    entities.append((ent.text,ent.label_,get_wikipedia_url(ent.text)))
-
-entities = list(set(entities))
-
-print("\n \n The recognized entites are: \n")
-
-for entity, category, wikipage in entities:
-    print(f"{entity}, {category}, {wikipage}")
+    # store the results in a file. 
+    with open('output.txt', 'w') as output_file:
+    for question_id, details in data.items():
+        output_file.write(f"{question_id}\tR\"{response}\"\n")
+        for entity in entities:
+            output_file.write(f"{question_id}\tE\"{entity}\"\n")
+   
